a93a310d8fcb49a2209094498755feec40ee55b2
Slawek Kolodziej <hfrntt@gmail.com>
2020-04-22 00:09:47 +0200

    Update apps

--- apps/app-1/index.js
+++ apps/app-1/index.js
@@ -3,4 +3,4 @@ const dep1 = require('dep-1')
 const dep2 = require('dep-2')
 
 fs.mkdirSync('./out');
-fs.writeFileSync('./out/index.html', `${dep1}, ${dep2}`)
+fs.writeFileSync('./out/index.html', `I am app 1 (data from deps: ${dep1}, ${dep2})`)
